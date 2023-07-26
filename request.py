3f04fa94a26c5136b2b6feef00632d43b5f89971
Mariano Isabello <nanoisabello@gmail.com>
2023-07-26 14:42:29 -0300

    xxx

--- request.py
+++ request.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+'''
 response = requests.get('http://www.omdbapi.com/?i=tt3896198&apikey=4c510e72')
 #response = requests.get("https://www.google.com")
 
@@ -13,9 +14,7 @@ elif response.status_code == 404:
     print("No se pude conectar")
 else:
     print("No se pudo conectar o recoletar datos")
-
-import requests
-
+'''
 #data = {'title': 'value1', 'year': 'value2','plot': 'value3', "Response":"value4"}
 params = {'t':'american pie'}
 response = requests.get('http://www.omdbapi.com/?i=tt3896198&apikey=4c510e72', params=params)
@@ -25,12 +24,18 @@ if response.status_code == 200:
     print("Conexion exitosa con la API")
     print('-------------------------------')
     data = response.json()
+    print(response.json())
+    print('-------------------------------')
 elif response.status_code == 404:
     print("No se pude conectar")
 else:
     print("No se pudo conectar o recoletar datos")
 
+
+"""""
 def new_func(response):
     print(response.json())
 
 new_func(response)
+
+"""""
